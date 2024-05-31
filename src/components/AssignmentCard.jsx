7f898d7fd6ce3e342905cf68a0be237580c5b012
Samboduy <samdanielsson000@gmail.com>
2024-05-31 15:18:11 +0200

    function name chagnes

--- src/components/AssignmentCard.jsx
+++ src/components/AssignmentCard.jsx
@@ -5,33 +5,33 @@ function AssignmentCard({ card }) {
     const { firebaseKey, assignment, assigned, category, status } = card;
     let tempName;
 
-    function handleInputAssignee(event) {
+    function assignedNameInput(event) {
         tempName = event.target.value;
         console.log(tempName);
     }
+
     //assigns someone to a assignment card, moves the card to done or deletes the card depending on status
-    function handleCardFormSubmit(event) {
+    function assignPersonToAssignment(event) {
         event.preventDefault();
         updateAssignment(firebaseKey, "in progress", tempName);
     }
 
-    function handleCardSubmit(event) {
+    function deleteOrChangeAssignmentStatus() {
         if (status == "in progress") {
             updateAssignment(firebaseKey, "done", assigned);
         } else if (status == "done") {
             deleteAssignment(firebaseKey);
         }
     }
+
     //shows the assignment card, changes the cards layout depending on it's status
-
-
     return (<div className={category.replace(' ', '-')}>
         <p>Assignment: {assignment}</p>
         {status != "to do" && <p>Assigned To:{assigned}</p>}
-        {status == "in progress" && <button onClick={handleCardSubmit}>Done {">>"}</button>}
-        {status == "done" && <button onClick={handleCardSubmit}>Remove X</button>}
-        {status == "to do" && <form onSubmit={handleCardFormSubmit}>
-            <input onChange={handleInputAssignee} required placeholder="Enter Name"></input>
+        {status == "in progress" && <button onClick={deleteOrChangeAssignmentStatus}>Done {">>"}</button>}
+        {status == "done" && <button onClick={deleteOrChangeAssignmentStatus}>Remove X</button>}
+        {status == "to do" && <form onSubmit={assignPersonToAssignment}>
+            <input onChange={assignedNameInput} required placeholder="Enter Name"></input>
             <button>Assign {">>"}</button>
         </form>}
     </div>
